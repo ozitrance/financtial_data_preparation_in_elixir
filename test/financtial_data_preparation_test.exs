62b677b3de42b5f7171a658d5ea2b25205818997
Yair Oz <ozyair85@gmail.com>
2024-10-28 16:04:28 -0700

    time_bars

---
+++ test/financtial_data_preparation_test.exs
@@ -0,0 +1,8 @@
+defmodule FinanctialDataPreparationTest do
+  use ExUnit.Case
+  doctest FinanctialDataPreparation
+
+  test "greets the world" do
+    assert FinanctialDataPreparation.hello() == :world
+  end
+end
